0f515a44ba79f59e61080c82d4b1e308b9ae751c
varenyamishra11 <varenyamishra11@gmail.com>
2021-06-26 19:09:27 +0530

    Project47

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,8 @@
 class Game{
     constructor(){
         this.gameBg=loadImage("Images/seaBg1.png");
+        this.introBg=loadImage("Images/Instructions_page(updated).png")
+        this.endBg=loadImage("Images/GameEndScreen.jpg")
 
         this.shellImg1=loadImage("Images/Shells/Shell1.png")
         this.shellImg2=loadImage("Images/Shells/Shell2.png")
@@ -22,9 +24,11 @@ class Game{
         this.coralImg1=loadImage("Images/Coral1.png")
         this.coralImg2=loadImage("Images/Coral2.png")
         this.weedImg=loadImage("Images/Sea weed.png")
-        this.paperDustbinImg=loadImage("Images/Waste/Paper_Dustbin.png")
+        this.binsImg=loadImage("Images/Waste/newBins.png")
+        /*this.paperDustbinImg=loadImage("Images/Waste/Paper_Dustbin.png")
         this.plasticDustbinImg=loadImage("Images/Waste/Plastic_Dustbin.png")
         this.glassDustbinImg=loadImage("Images/Waste/Glass_Dustbin.png")
+        this.metalDustbinImg=loadImage("Images/Waste/metalBin.png")*/
 
         this.PlasticBottleImg=loadImage("Images/Waste/plasticBottle.png");
         //this.bluePlasticImg=loadImage("Images/Waste/bluePlastic_bag.png");
@@ -38,31 +42,82 @@ class Game{
         this.player.setCollider("circle",35,90,20)
         this.playerImg=loadImage("Images/girl2CollectingShellsNoBg.png")
         this.player.addImage("player",this.playerImg);
-        this.plasticDustbin=createSprite(1019,565);
+        this.bins=createSprite(1120,557);
+        this.bins.addImage("Dustbins",this.binsImg);
+        this.bins.scale=1.8
+       /* this.plasticDustbin=createSprite(1019,565);
         this.paperDustbin=createSprite(1163,557);
         this.glassDustbin=createSprite(1307,557);
+        this.metalDustbin=createSprite(1219,557);
         this.paperDustbin.addImage("paper",this.paperDustbinImg);
         this.glassDustbin.addImage("glass",this.glassDustbinImg);
         this.plasticDustbin.addImage("plastic",this.plasticDustbinImg);
-        this.plasticDustbin.scale=0.5;
+        this.metalDustbin.addImage("Metal",this.metalDustbinImg);
+        this.plasticDustbin.scale=0.5;*/
 
         this.shellGroup=new Group();
         this.coralGroup=new Group();
         this.weedGroup=new Group();
-        this.plasticGroup=new Group();
-        this.metalGroup=new Group();
+        this.plastic1Group=new Group();
+        this.plastic2Group=new Group();
+        this.metal1Group=new Group();
+        this.metal2Group=new Group();
         this.glassGroup=new Group();
 
-      
+        this.Plastic1Btn=createButton("Dispose bottle in 'Plastic' Bin")
+        this.Plastic1Btn.position(593,118)
+        this.Plastic1Btn.hide();
+
+        this.Plastic2Btn=createButton("Dispose polythene in 'Plastic' Bin")
+        this.Plastic2Btn.position(593,118)
+        this.Plastic2Btn.hide();
+
+        this.Metal1Btn=createButton("Dispose can in 'Metal' Bin")
+        this.Metal1Btn.position(593,118)
+        this.Metal1Btn.hide();
+
+        this.Metal2Btn=createButton("Dispose can in 'Metal' Bin")
+        this.Metal2Btn.position(593,118)
+        this.Metal2Btn.hide();
+
+        this.GlassBtn=createButton("Dispose bottle in 'Glass' Bin")
+        this.GlassBtn.position(593,118)
+        this.GlassBtn.hide();
+
     }
+
+    
 
     play(){
         background(this.gameBg);
     }
 
+    introduce(){
+        background(this.introBg);
+    }
+
+    end(){
+        background(this.endBg);
+    }
+
+    scoreCondition(){
+        if(score>=100 && score<=106){
+            score=score+25;
+            swal({
+                title: "That's Awesome",
+                text: "You got Bonus of +25 points!!",
+                icon: "success",
+                button: "Oh Yess!!",
+              });
+        }
+
+    }
+        
+        
+
     spawnShells(){
 
-       if(frameCount%200===0){
+       if(frameCount%150===0){
         var shell = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         shell.debug=true;
         var rand = Math.round(random(1,17));
@@ -107,9 +162,14 @@ class Game{
                 break;
         }
         shell.scale=0.2;
-
-    this.shellGroup.add(shell);
+        this.shellGroup.add(shell);
        }
+
+        for (let index = 0; index <this.shellGroup.length; index++) {
+            if(frameCount%1150===0){
+                this.shellGroup[index].destroy();
+            }
+        }
 
        if(frameCount%1200===0){
         var coral1 = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
@@ -127,6 +187,12 @@ class Game{
         this.coralGroup.add(coral2);  
        }
 
+       for (let index = 0; index <this.coralGroup.length; index++) {
+        if(frameCount%2525===0){
+            this.coralGroup[index].destroy();
+        }
+    }
+
     if(frameCount%700===0){
         var weed = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         weed.debug=true;
@@ -135,13 +201,25 @@ class Game{
         this.weedGroup.add(weed);    
        }
 
-    if(frameCount%850===0){
+       for (let index = 0; index <this.weedGroup.length; index++) {
+        if(frameCount%1535===0){
+            this.weedGroup[index].destroy();
+        }
+    }
+
+    if(frameCount%1350===0){
         var plasticBottle = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         plasticBottle.debug=true;
         plasticBottle.addImage("plasticBottle",this.PlasticBottleImg); 
         plasticBottle.scale=0.1;  
-        this.plasticGroup.add(plasticBottle);    
+        this.plastic1Group.add(plasticBottle);    
        }
+
+       for (let index = 0; index <this.plastic1Group.length; index++) {
+        if(frameCount%2882===0){
+            this.plastic1Group[index].destroy();
+        }
+    }
 
        /*if(frameCount%939===0){
         var bluePlastic = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
@@ -151,37 +229,61 @@ class Game{
         this.wasteGroup.add(bluePlastic);    
        }*/
 
-       if(frameCount%847===0){
+       if(frameCount%1447===0){
         var yellowPlastic = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         yellowPlastic.debug=true;
         yellowPlastic.addImage("yellowPlastic",this.yellowPlasticImg); 
         yellowPlastic.scale=0.2;  
-        this.plasticGroup.add(yellowPlastic);    
+        this.plastic2Group.add(yellowPlastic);    
        }
 
-       if(frameCount%917===0){
+       for (let index = 0; index <this.plastic2Group.length; index++) {
+        if(frameCount%2424===0){
+            this.plastic2Group[index].destroy();
+        }
+    }
+
+       if(frameCount%1417===0){
         var crushedCan = createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         crushedCan.debug=true;
         crushedCan.addImage("Crushed can",this.crushedCanImg); 
         crushedCan.scale=0.1;  
-        this.metalGroup.add(crushedCan);    
+        this.metal1Group.add(crushedCan);    
        }
 
-       if(frameCount%923===0){
+       for (let index = 0; index <this.metal1Group.length; index++) {
+        if(frameCount%2424===0){
+            this.metal1Group[index].destroy();
+        }
+    }
+
+       if(frameCount%1323===0){
         var AluminiumCan= createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         AluminiumCan.debug=true;
         AluminiumCan.addImage("Aluminium can",this.AluminiumCanImg); 
         AluminiumCan.scale=0.1;  
-        this.metalGroup.add(AluminiumCan);    
+        this.metal2Group.add(AluminiumCan);    
        }
+
+       for (let index = 0; index <this.metal2Group.length; index++) {
+        if(frameCount%2999===0){
+            this.metal2Group[index].destroy();
+        }
+    }
        
-       if(frameCount%1067===0){
+       if(frameCount%1567===0){
         var glassBottle= createSprite(random(300,windowWidth-50),random(windowHeight-180,windowHeight-300));
         glassBottle.debug=true;
         glassBottle.addImage("Glass Bottle",this.glassBottleImg); 
         glassBottle.scale=0.2;  
         this.glassGroup.add(glassBottle);    
        }
+
+       for (let index = 0; index <this.glassGroup.length; index++) {
+        if(frameCount%3003===0){
+            this.glassGroup[index].destroy();
+        }
+    }
        
         drawSprites(); 
     }
@@ -209,10 +311,114 @@ class Game{
             if(this.weedGroup[index].isTouching(this.player)){
                  score=score-4;
                 this.weedGroup[index].destroy();
-            }
-            
+            }      
         }
-    }
+
+        for (let index = 0; index <this.plastic1Group.length; index++) {
+            if(this.plastic1Group[index].isTouching(this.player)){
+                this.Plastic1Btn.show();
+                this.btnStyle()
+                console.log(this.plastic1Group.length);
+                this.Plastic1Btn.mousePressed(()=>{
+                    this.plastic1Group[index].destroy();
+                    this.Plastic1Btn.hide();
+                    //var R = Math.round(random(0,2));
+                /*setTimeout(() => {
+                    textSize(30);
+                    text("Bravo!!Hats off to you",934,52)
+                }, 1000);*/
+                    score=score+5;
+                })
+            }
+            else{
+                this.Plastic1Btn.hide();
+            }
+        }
+
+        for (let index = 0; index <this.plastic2Group.length; index++) {
+            if(this.plastic2Group[index].isTouching(this.player)){
+                this.Plastic2Btn.show();
+                this.btnStyle()
+                console.log(this.plastic2Group.length);
+                this.Plastic2Btn.mousePressed(()=>{
+                    this.plastic2Group[index].destroy();
+                    this.Plastic2Btn.hide();
+                    //var Ra = Math.round(random(0,2));
+                /*setTimeout(() => {
+                    textSize(30);
+                    text("Good job :D. Did you know? 8 million pieces of plastic pollution find their way into our ocean daily.",934,52)
+                }, 10000);*/
+                    score=score+5;
+                })
+            }
+            else{
+                this.Plastic2Btn.hide();
+            }
+        }
+
+        for (let index = 0; index <this.metal1Group.length; index++) {
+            if(this.metal1Group[index].isTouching(this.player)){
+                this.Metal1Btn.show();
+                this.btnStyle()
+                console.log(this.metal1Group.length);
+                this.Metal1Btn.mousePressed(()=>{
+                    this.metal1Group[index].destroy();
+                    this.Metal1Btn.hide();
+                    //var Ran = Math.round(random(0,2));
+                /*setTimeout(() => {
+                    textSize(30);
+                    text("Bravo👏👏. Did you know? 79% of plastic waste is sent to landfills or the ocean, while only 9% is recycled, and 12% gets incinerated. ",934,52)
+                }, 6000);*/
+                    score=score+5;
+                })
+            }
+            else{
+                this.Metal1Btn.hide();
+            }
+        }
+
+        for (let index = 0; index <this.metal2Group.length; index++) {
+            if(this.metal2Group[index].isTouching(this.player)){
+                this.Metal2Btn.show();
+                this.btnStyle()
+                console.log(this.metal2Group.length);
+                this.Metal2Btn.mousePressed(()=>{
+                    this.metal2Group[index].destroy();
+                    this.Metal2Btn.hide();
+                    //var Rand = Math.round(random(0,2));
+                //setTimeout(() => {
+                    /*textSize(30);
+                    text("Good job!!You are helping the Environment immensely",934,52)
+               // }, 2000);*/
+                    score=score+5;
+                })
+            }
+            else{
+                this.Metal2Btn.hide();
+            }
+        }        
+
+        for (let index = 0; index <this.glassGroup.length; index++) {
+            if(this.glassGroup[index].isTouching(this.player)){
+                this.GlassBtn.show();
+                this.btnStyle()
+                console.log(this.glassGroup.length);
+                this.GlassBtn.mousePressed(()=>{
+                    this.glassGroup[index].destroy();
+                    this.GlassBtn.hide();
+                    //var Rando = Math.round(random(0,2));
+               /* setTimeout(() => {
+                    textSize(30);
+                    text("Excellent👏👏you are inspiring others too",934,52)
+                }, 6000);*/
+                    score=score+5;
+                })
+            }
+            else{
+                this.GlassBtn.hide();
+            }
+        }
+       }
     moveLeft(){
         this.player.x=this.player.x-5
     }
@@ -225,31 +431,12 @@ class Game{
     moveUp(){
         this.player.y=this.player.y-5
     }
-    dropWaste(){
-        /*if(this.plasticGroup.isTouching(this.player)){
-            this.plasticGroup.x=this.player.x;
-            this.plasticGroup.y=this.player.y;
-            if(this.plasticGroup.isTouching(this.plasticDustbin)){
-                score=score+5
-                this.plasticGroup[index].destroy();
-            }
-        }*/
 
-
-        for (let index = 0; index <this.plasticGroup.length; index++) {
-            if(this.plasticGroup[index].isTouching(this.player)){
-                PlasticBtn.show();
-                 //score=score+5;
-                //this.plasticGroup[index].destroy();
-            }
-            
-        }
+    btnStyle(){
+        this.Plastic1Btn.class("PBtn");
+        this.Plastic2Btn.class("PBtn");
+        this.Metal1Btn.class("MBtn");
+        this.Metal2Btn.class("MBtn"); 
+        this.GlassBtn.class("GBtn");
     }
-
-    plasticDisposed() {
-  for (let index = 0; index <this.plasticGroup.length; index++) {
-    this.plasticGroup[index].destroy();
-  }
-}
-    
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var WELCOME = 0;
 var PLAY = 1;
 var END = 2;
+var INTRO = 3;
 var gameState = WELCOME;
 
 var score=0;
@@ -8,6 +9,7 @@ var welcome_screen , game;
 var shellsGroup;
 var plasticDustbin , glassDustbin , PaperDustbin;
 var PlasticBtn;
+var message=['Good Job!! You r helping the Environment','B','C']
 
 function preload(){
 
@@ -18,10 +20,7 @@ function setup() {
   game = new Game();
   welcome_screen = new WelcomeScreen();
 
-  PlasticBtn=createButton("dispose waste in Plastic dustbin")
-  PlasticBtn.position(593,118)
-  //PlasticBtn.mousePressed(game.plasticDisposed)
-  PlasticBtn.hide();
+
   
 }
 
@@ -36,19 +35,28 @@ function draw() {
     fill(255)
     text("Press SPACE to start game", width/2-170, height-200);
     if(keyDown("space")){
-      gameState = PLAY;
-      
+      gameState = INTRO; 
+    }
+  }
+  else if(gameState===INTRO){
+    game.introduce();
+    if(keyDown("enter")){
+      gameState= PLAY;
     }
   }
   else if(gameState === PLAY){
+      //playSound("seaWaves.wav")
       game.play();
       game.spawnShells();
       game.calculateScore();
-      game.dropWaste();
       textSize(35);
       textFont("Cavolini");
       fill(244, 75, 177);
       text("Score:"+score,640,64);
+      game.scoreCondition();
+      if (score>=300) {
+        gameState=END
+      }
       if (keyDown(LEFT_ARROW)) {
           game.moveLeft();        
       }
@@ -66,4 +74,8 @@ function draw() {
         }
 
   }
+  else if(gameState===END){
+    game.end();
+   
+}
 }
